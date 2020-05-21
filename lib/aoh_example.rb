c046385f59060aa2954d924e454cf34b5bca0c64
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-21 19:50:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,6 +26,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   assembled_aoh[row][key] = new_value
+  assembled_aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
