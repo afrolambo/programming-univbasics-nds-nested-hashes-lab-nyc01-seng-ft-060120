b44014e55562ee3c8fc019924432847cea672c1d
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-21 19:44:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  assembled_aoh(DON_G.sort,JOELLE_VD.sort,PAT_M.sort,KATE_G.sort,BRUCE_G.sort)
+  [DON_G.sort,JOELLE_VD.sort,PAT_M.sort,KATE_G.sort,BRUCE_G.sort]
 end
 
 def aoh_lookup(aoh, row, key)
